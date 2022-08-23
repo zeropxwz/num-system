2e8868c61842607ed8fb58aadb49cfb8671c1634
zerop <zerop.development@gmail.com>
2022-08-23 04:28:58 +0300

    add function convet from other to decimal system

--- public/main.js
+++ public/main.js
@@ -1,12 +1,21 @@
 "use strict";
-function toOtherSystem(num, sys) {
-    let res = '';
-    while (num >= sys) {
-        res += String(Math.floor(num % sys));
-        num = Number(Math.floor(num / sys));
+// function toOther (num: number, sys: number): string {
+//     let res: string = ''
+//     while (num >= sys) {
+//         res += String(Math.floor(num % sys))
+//         num  = Number(Math.floor(num / sys))
+//     }
+//     res += String(num)
+//     return res.split('').reverse().join().replace(/,/g, '') 
+// }
+// console.log(
+//     toOther(5, 2) // вывидет 101
+// )
+function toDecimal(num, sys) {
+    let res = 0;
+    for (let i = 0; i < num.length; i++) {
+        res += Number(num[i]) * Math.pow(sys, num.length - i - 1);
     }
-    res += String(num);
-    return res.split('').reverse().join().replace(/,/g, '');
+    return res;
 }
-console.log(toOtherSystem(5, 2) // вывидет 101
-);
+console.log(toDecimal('101', 2));
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,34 @@
+// function toOther (num: number, sys: number): string {
+
+//     let res: string = ''
+
+//     while (num >= sys) {
+//         res += String(Math.floor(num % sys))
+//         num  = Number(Math.floor(num / sys))
+//     }
+
+//     res += String(num)
+
+//     return res.split('').reverse().join().replace(/,/g, '') 
+// }
+
+// console.log(
+//     toOther(5, 2) // вывидет 101
+// )
 
 
 
+function toDecimal(num: string, sys: number): number {
 
+    let res: number = 0
 
-
-
-
-
-function toOtherSystem (num: number, sys: number): string {
-
-    let res: string = ''
-
-    while (num >= sys) {
-        res += String(Math.floor(num % sys))
-        num  = Number(Math.floor(num / sys))
+    for (let i = 0; i < num.length; i++) {
+        res += Number(num[i]) * Math.pow(sys, num.length - i - 1)
     }
 
-    res += String(num)
-
-    return res.split('').reverse().join().replace(/,/g, '') 
+    return res
 }
 
 console.log(
-    toOtherSystem(5, 2) // вывидет 101
+    toDecimal('101', 2)
 )
